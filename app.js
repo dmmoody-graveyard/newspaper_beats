070944f21abcfedc1e6cc13bdfe8cea65ed79530
Duane M. Moody <dmmoody@gmail.com>
2015-02-27 11:18:16 -0800

    add new reporters form and display reporter list

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var newspaperBeats = angular.module('newspaperBeats', ['ui.router']);
 newspaperBeats.config(function($stateProvider) {
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "partials/home.html",
-    controller: "newspaperBeatsCtrl"
+    templateUrl: "partials/home.html"
   });
 });
