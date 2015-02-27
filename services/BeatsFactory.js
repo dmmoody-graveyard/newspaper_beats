333c9818f883b952db06ddc2c9a9fb8cc27526c5
Duane M. Moody <dmmoody@gmail.com>
2015-02-27 15:00:02 -0800

    add ability to assign reporters to beats

--- services/BeatsFactory.js
+++ services/BeatsFactory.js
@@ -2,8 +2,10 @@ newspaperBeats.factory('BeatsFactory', function BeatsFactory() {
   var factory = {};
   factory.beats = [];
   factory.addNewBeat = function() {
-    factory.beats.push({ name: factory.beatName });
+    factory.beats.push({ name: factory.beatName, reporters: factory.reportersAssigned });
+    debugger;
     factory.beatName = null;
+    factory.reportersAssigned = null;
   };
 
   return factory;
