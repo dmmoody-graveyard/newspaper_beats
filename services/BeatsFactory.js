08fe2621faf57beaf1d067be1230602879cef426
Duane M. Moody <dmmoody@gmail.com>
2015-02-27 10:53:59 -0800

    add form to enter new beats

--- services/BeatsFactory.js
+++ services/BeatsFactory.js
@@ -2,7 +2,7 @@ newspaperBeats.factory('BeatsFactory', function BeatsFactory() {
   var factory = {};
   factory.beats = [];
   factory.addNewBeat = function() {
-    factory.beats.push({ name: beatName });
+    factory.beats.push({ name: factory.beatName });
     factory.beatName = null;
   };
 
